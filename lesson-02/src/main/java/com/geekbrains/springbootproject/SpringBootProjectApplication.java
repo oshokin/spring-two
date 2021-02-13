76c4c0008a00b9bfe3dbfd2704e36185314ae849
Oleg Shokin <shokinoleg@gmail.com>
2021-02-13 21:01:57 +0300

    Добавлен исходный код 2го урока

---
+++ lesson-02/src/main/java/com/geekbrains/springbootproject/SpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.geekbrains.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+	// общий шаблон thymeleaf
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+	}
+}
---
+++ lesson-02/src/main/java/com/geekbrains/springbootproject/controllers/ShoppingCartController.java
@@ -0,0 +1,36 @@
+package com.geekbrains.springbootproject.controllers;
+
+import com.geekbrains.springbootproject.utils.ShoppingCart;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/shop/cart")
+public class ShoppingCartController {
+    private ShoppingCart shoppingCart;
+
+    @Autowired
+    public void setShoppingCartService(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+
+    @GetMapping
+    public String cartPage(Model model) {
+        model.addAttribute("cart", shoppingCart);
+        return "cart-page";
+    }
+
+    @GetMapping("/add/{id}")
+    public String addProductToCart(@PathVariable("id") Long id, HttpServletRequest httpServletRequest) {
+        shoppingCart.add(id);
+        String referrer = httpServletRequest.getHeader("referer");
+        return "redirect:" + referrer;
+    }
+}
